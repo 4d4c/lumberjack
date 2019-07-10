9ce214f69a5f64278a58c60bece698828932f2f6
4d4c <luferovsm@gmail.com>
2019-07-10 23:46:30 +0100

    Version 0.1

--- lumberjack.py
+++ lumberjack.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 
 from termcolor import colored
@@ -6,7 +7,7 @@ from termcolor import colored
 class lumberjack():
     def __init__(self, log_file):
         try:
-            self.log_file = open(log_file, "w+")
+            self.log_file = open(log_file, "a")
         except FileNotFoundError:
             print(colored("[-] ERROR: File not found: " + log_file, "red"))
             sys.exit(1)
@@ -15,20 +16,30 @@ class lumberjack():
             sys.exit(1)
 
 
+    def print_message(self, prefix, message, color):
+        full_message = "[{}] [{}] {}".format(
+            prefix,
+            datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            message
+        )
+
+        print(colored(full_message, color))
+
+        self.log_file.write(full_message.strip() + "\n")
+
+
     def log_error(self, message):
-        pass
+        self.print_message("-", message, "red")
 
 
     def log_warning(self, message):
-        pass
+        self.print_message("!", message, "yellow")
 
 
     def log_info(self, message):
-        pass
+        self.print_message("+", message, "green")
 
 
     def log_debug(self, message):
-        pass
+        self.print_message("D", message, "cyan")
 
-
-Lumberjack("/tmp/test")
